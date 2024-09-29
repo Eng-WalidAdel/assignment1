647b76ca8249afa8827443452b881ecc3069c599
ENG- Walid Adel <engwalidadel2005@gmail.com>
2024-09-29 21:13:14 +0300

    created the environment workspace to be ready

---
+++ task1.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+#define endl '\n'
+#define loop(i, n) for (int i = 0; i < n; i++)
+#define loop2(i, n) for (int i = 1; i <= n; i++)
+#define testcases \
+    int t;        \
+    cin >> t;     \
+    while (t--)
+#define loop_cout(d) for (auto it = d.begin(); it != d.end(); it++)
+#define ll long long
+#define endl '\n'
+using namespace std;
+void fast()
+{
+    ios_base::sync_with_stdio(0), cin.tie(0), cout.tie(0);
+}
+void gamal1();
+void gamal2();
+void gamal3();
+void gamal4();
+
+void mostafa1();
+void mostafa2();
+void mostafa3();
+void mostafa4();
+
+void walid1();
+void walid2();
+void walid3();
+void walid4();
+
+int main(){
+    return 0;
+}
