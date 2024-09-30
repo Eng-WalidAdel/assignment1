45a1bf749d764be3b3ce2ea54db7634fe0ba50e3
ENG- Walid Adel <engwalidadel2005@gmail.com>
2024-09-30 19:38:54 +0300

    my last problem has been done

--- task1.cpp
+++ task1.cpp
@@ -1,4 +1,11 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <unordered_map>
+#include <sstream>
+#include <vector>
+#include <map>
+#include <algorithm>
 #define endl '\n'
 using namespace std;
 void fast()
@@ -10,7 +17,7 @@ void Sieve();
 void dominos();
 void Bias();
 
-void inclusive_speech_processor ();
+void inclusive_speech_processor();
 void mostafa2();
 void mostafa3();
 void mostafa4();
@@ -18,28 +25,31 @@ void mostafa4();
 vector<string> split(string sentence, string deli);
 void binaryPrint(int n);
 static bool bears(int n);
-void walid4();
+void phisshing_emails();
 
 int main()
 {
-
+    phisshing_emails();
     return 0;
 }
 
 // ================================ GAMAL =======================================
 
-void sentence() {
-    string ans , s;
-    getline(cin , s);
-    for (int i = 0; i < s.size(); ++i) {
-        if(s[i] != ' ')
+void sentence()
+{
+    string ans, s;
+    getline(cin, s);
+    for (int i = 0; i < s.size(); ++i)
+    {
+        if (s[i] != ' ')
         {
             ans += tolower(s[i]);
         }
         else
         {
             ans += s[i];
-            while (s[i] == ' '){
+            while (s[i] == ' ')
+            {
                 i++;
             }
             ans += tolower(s[i]);
@@ -49,27 +59,34 @@ void sentence() {
     cout << ans << '\n';
 }
 
-void Sieve() {
+void Sieve()
+{
     int n;
     cin >> n;
     vector<bool> isPrime(n + 1, true);
     isPrime[0] = isPrime[1] = false;
 
-    for (int i = 2; i * i <= n; ++i) {
-        if (isPrime[i]) {
-            for (int j = i * i; j <= n; j += i) {
+    for (int i = 2; i * i <= n; ++i)
+    {
+        if (isPrime[i])
+        {
+            for (int j = i * i; j <= n; j += i)
+            {
                 isPrime[j] = false;
             }
         }
     }
-    for (int i = 2; i <= n; ++i) {
-        if (isPrime[i]) {
+    for (int i = 2; i <= n; ++i)
+    {
+        if (isPrime[i])
+        {
             cout << i << ' ';
         }
     }
 }
 
-struct dominoT {
+struct dominoT
+{
     int leftDots;
     int rightDots;
 };
@@ -77,44 +94,49 @@ void dominos()
 {
     int n;
     cin >> n;
-    vector<dominoT>d(n);
-    vector<int>prem(n);
-    for (int i = 0; i < n; ++i) {
+    vector<dominoT> d(n);
+    vector<int> prem(n);
+    for (int i = 0; i < n; ++i)
+    {
         prem[i] = i;
         cin >> d[i].leftDots >> d[i].rightDots;
     }
 
-    do {
+    do
+    {
         bool valid_chain = true;
         valid_chain &= d[prem[0]].rightDots == d[prem[1]].leftDots;
-        for (int i = 1; i < n - 1; ++i) {
+        for (int i = 1; i < n - 1; ++i)
+        {
             valid_chain &= d[prem[i]].leftDots == d[prem[i - 1]].rightDots;
             valid_chain &= d[prem[i]].rightDots == d[prem[i + 1]].leftDots;
         }
         valid_chain &= d[prem[n - 1]].leftDots == d[prem[n - 2]].rightDots;
-        if(valid_chain)
+        if (valid_chain)
         {
-            for (int i = 0; i < n; ++i) {
-                cout << d[prem[i]].leftDots << '|' << d[prem[i]].rightDots << ((i == n - 1)? "\n" : " - ");
+            for (int i = 0; i < n; ++i)
+            {
+                cout << d[prem[i]].leftDots << '|' << d[prem[i]].rightDots << ((i == n - 1) ? "\n" : " - ");
             }
             return;
         }
-    } while (next_permutation(prem.begin() , prem.end()));
+    } while (next_permutation(prem.begin(), prem.end()));
 
     cout << "Impossible" << '\n';
 }
 
 void Bias()
 {
-    unordered_map<string , string>alter;
+    unordered_map<string, string> alter;
     ifstream alter_file("alter.txt");
     string exp;
-    while (getline(alter_file , exp))
+    while (getline(alter_file, exp))
     {
-        string key , value;
+        string key, value;
         bool v = false;
-        for (int i = 0; i < exp.size(); ++i) {
-            if(!isspace(exp[i]))
+        for (int i = 0; i < exp.size(); ++i)
+        {
+            if (!isspace(exp[i]))
             {
                 ((v) ? key += exp[i] : value += exp[i]);
             }
@@ -129,7 +151,7 @@ void Bias()
     int cnt = 0;
     while (cin >> word)
     {
-        if(alter.find(word) == alter.end())
+        if (alter.find(word) == alter.end())
         {
             cout << word << ' ';
         }
@@ -139,7 +161,7 @@ void Bias()
             cout << alter[word] << ' ';
         }
     }
-//    cout << cnt << '\n';
+    //    cout << cnt << '\n';
 }
 
 // ================================== WALEAD ==============================================
@@ -201,62 +223,108 @@ static bool bears(int n)
         }
         if ((n % 3 == 0) || (n % 4 == 0))
         {
-            if (((n % 10) * (n % 100)) >0 && bears(n - ((n % 10) * ((n % 100)/10))))
+            if (((n % 10) * (n % 100)) > 0 && bears(n - ((n % 10) * ((n % 100) / 10))))
                 return true;
         }
     }
     return false;
 }
+void phisshing_emails()
+{
+    unordered_map<string, int> phishing = {{"secure", 2}, {"transaction", 2}, {"login", 2}, {"credit card", 3}, {"identity", 2}, {"update", 2}, {"security alert", 3}, {"bank", 1}, {"invoice", 2}, {"winner", 3}, {"verify your identity", 3}, {"confirm", 2}, {"refund", 2}, {"limited time", 2}, {"offer", 2}, {"lottery", 3}, {"free", 2}, {"urgent", 3}, {"password", 3}, {"reset", 2}, {"click here", 3}, {"verify", 3}, {"account", 2}, {"suspended", 3}, {"limited", 2}, {"social security", 3}, {"prize", 3}, {"you have won", 3}, {"claim", 3}};
+    int totalpoints = 0;
+    map<string, int> shared_words;
+    string filename;
+    cout << "PLease, Enter a file name: ";
+    cin >> filename;
+    ifstream file(filename);
+    string line;
+    while (getline(file, line))
+    {
+        for (char &ch : line)
+        {
+            ch = tolower(ch);
+        }
+        for (auto it = phishing.begin(); it != phishing.end(); it++)
+        {
+            short pos = line.find(it->first);
+            while (pos != string::npos)
+            {
+                shared_words[it->first]++;
+                totalpoints += it->second;
+                pos = line.find(it->first, pos + it->first.length());
+            }
+        }
+    }
+    file.close();
+    cout << "The results of the scanner: ";
+    for (auto it = shared_words.begin(); it != shared_words.end(); it++)
+    {
+        cout << "phisshing word: [ " << it->first << " ] appeared ( " << it->second << " ) times in that email." << endl;
+    }
+    cout << endl
+         << "Total points of phishing words in this email: " << totalpoints << endl;
+}
 // =========================== Mostafa ==========================================
 
-void inclusive_speech_processor (){
+void inclusive_speech_processor()
+{
 
-    string sentence , word , inclusive_speech;
+    string sentence, word, inclusive_speech;
     // 'word' holds each word being processed; 'inclusive_speech' stores the modified sentence.
 
     getline(cin, sentence); // Read the input sentence from the user.
 
-
     // Loop through each character in the input sentence.
-    for (char letter : sentence){
+    for (char letter : sentence)
+    {
 
         // Check if the current character is a separator (punctuation or space).
-        if ( letter == ' ' || letter == '?' || letter == ',' || letter == '.' ||
-             letter == ';' || letter == ':' || letter == '!' || letter == '-' ||
-             letter == '"' || letter == '/' || letter == '(' || letter == ')' ){
+        if (letter == ' ' || letter == '?' || letter == ',' || letter == '.' ||
+            letter == ';' || letter == ':' || letter == '!' || letter == '-' ||
+            letter == '"' || letter == '/' || letter == '(' || letter == ')')
+        {
 
             // Check if the current word is a gender-specific pronoun and replace it.
-            if ( word == "he" || word == "she" ){
+            if (word == "he" || word == "she")
+            {
                 inclusive_speech += "he or she"; // Replace "he" or "she" with "he or she".
             }
 
-            else if ( word == "He" || word == "She" ){
+            else if (word == "He" || word == "She")
+            {
                 inclusive_speech += "He or She"; // Replace "He" or "She" with "He or She".
             }
 
-            else if ( word == "him" || word == "her" ){
+            else if (word == "him" || word == "her")
+            {
                 inclusive_speech += "him or her"; // Replace "him" or "her" with "him or her".
             }
 
-            else if ( word == "Him" || word == "Her" ){
+            else if (word == "Him" || word == "Her")
+            {
                 inclusive_speech += "Him or Her"; // Replace "Him" or "Her" with "Him or Her".
             }
 
-            else if ( word == "his" || word == "hers" ){
+            else if (word == "his" || word == "hers")
+            {
                 inclusive_speech += "his or hers"; // Replace "his" or "hers" with "his or hers".
             }
 
-            else if ( word == "His" || word == "Hers" ){
+            else if (word == "His" || word == "Hers")
+            {
                 inclusive_speech += "His or Hers"; // Replace "His" or "Hers" with "His or Hers".
             }
 
-            else if ( word == "himself" || word == "herself" ){
+            else if (word == "himself" || word == "herself")
+            {
                 // Replace "himself" or "herself" with "himself or herself".
                 // Note: It's assumed "himself" or "herself" won't start a sentence.
                 inclusive_speech += "himself or herself";
             }
 
-            else {
+            else
+            {
                 // If the word is not a gender-specific pronoun, add it to the result unchanged.
                 inclusive_speech += word;
             }
@@ -266,7 +334,8 @@ void inclusive_speech_processor (){
             word.clear(); // Clear the current word to start building the next word.
         }
 
-        else {
+        else
+        {
             // If the current character is not punctuation or a space, it's part of a word.
             word += letter; // Add the character to the current word.
         }
