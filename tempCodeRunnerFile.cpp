198c0d04bcba7e4823bb50f2702e68448610e7c9
ENG- Walid Adel <khattabadel112005@gmail.com>
2024-09-30 19:52:15 +0300

    Update tempCodeRunnerFile.cpp

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,4 +1,1 @@
-   for (auto it = shared_words.begin(); it != shared_words.end(); it++)
-    {
-        cout << "phisshing word: [ " << it->first << " ] appeared ( " << it->second << " ) times in that email." << endl;
-    }
+
